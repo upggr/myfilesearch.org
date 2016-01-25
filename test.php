2e2924092305d3a8c9b906dc1d4f4ad999b98c5e
Ioannis Kokkinis <info@upg.gr>
2016-01-25 21:10:00 +0000

    test

--- test.php
+++ test.php
@@ -1,3 +1,8 @@
 
-<?php include('functions.php');?>
-<?php scrapmagnetsite('https://kat.cr/usearch/lord/');//} ?>
+<?php 
+ini_set('display_errors', '1');
+error_reporting(E_ALL); 
+include('functions.php');
+echo 'a';
+scrapmagnetsite('https://kat.cr/usearch/lord/');
+?>
