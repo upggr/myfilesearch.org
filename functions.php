e71c3f207b0308dfe9c8cff2ccb7c008ccb42786
Ioannis Kokkinis <info@upg.gr>
2016-01-26 20:11:26 +0000

    f

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
-//ini_set('display_errors', '1');
-//error_reporting(E_ALL); 
+ini_set('display_errors', '1');
+error_reporting(E_ALL); 
 
 function scraplatestsearchesfromkickass() {
 	$theurl = 'https://kat.cr/latest-searches/';
