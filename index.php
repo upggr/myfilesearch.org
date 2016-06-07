9c6cd11c3938f0a9a4cba057a7a0dc6b65abd524
Ioannis Kokkinis <info@upg.gr>
2016-06-08 00:54:45 +0300

    Updated index.php.

--- index.php
+++ index.php
@@ -81,6 +81,7 @@
                <?php if (isset($_POST['s'])) {
     ?> <h3 class="mbr-header__text">Results : <?php echo $_POST['s'];
     ?></h3><?php
+
 };?>
 
             </div>
@@ -148,23 +149,7 @@
   <script src="assets/mobirise/js/script.js"></script>
   <script src="assets/formoid/formoid.min.js"></script>
 
-<!-- Start of StatCounter Code for Default Guide -->
-<script type="text/javascript">
-var sc_project=10858297;
-var sc_invisible=0;
-var sc_security="02e5deb8";
-var scJsHost = (("https:" == document.location.protocol) ?
-"https://secure." : "http://www.");
-document.write("<sc"+"ript type='text/javascript' src='" +
-scJsHost+
-"statcounter.com/counter/counter.js'></"+"script>");
-</script>
-<noscript><div class="statcounter"><a title="shopify stats"
-href="http://statcounter.com/shopify/" target="_blank"><img
-class="statcounter"
-src="http://c.statcounter.com/10858297/0/02e5deb8/0/"
-alt="shopify stats"></a></div></noscript>
-<!-- End of StatCounter Code for Default Guide -->
+
 
 
 
